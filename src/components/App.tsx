55e66beb1d2f3240e555a015534796d6c6ef341b
Leftyxiv <manny@aledoux.net>
2021-08-24 14:28:28 -0400

    wired everything together

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-const App: React.FC = (): JSX.Element => {
-  return (
-    <div>
-      Hi there!
-    </div>
-  )
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
 }
 
-export default App;
+const _App: React.FC<AppProps> = (): JSX.Element => {
+  return <div>Hi there!</div>;
+};
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
