004164af71415fc7ed06b3d8e3969adc4fdc154f
Leftyxiv <manny@aledoux.net>
2021-08-24 15:40:32 -0400

    added delete todo functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
   fetchTodos(): any;
+  deleteTodo(id: number): any;
 }
 
 const _App: React.FC<AppProps> = (props: AppProps): JSX.Element => {
   const onButtonClick = (): void => {
     props.fetchTodos();
   };
+  const onTodoClick = (id: number): void => {
+    props.deleteTodo(id);
+  };
   const renderList = (): JSX.Element[] => {
     return props.todos.map((todo: Todo) => {
-      return <li key={todo.id}>{todo.title}</li>;
+      return <li key={todo.id} onClick={() => onTodoClick(todo.id)}>{todo.title}</li>;
     });
   };
   return <div>
@@ -29,4 +33,4 @@ const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
   return { todos: state.todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
